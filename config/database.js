5c5ac46b4e45f49198ef9cdf793f2279d6a336a7
Alisson <021.889528@universidadesenaicimatec.edu.br>
2025-11-14 16:16:16 -0300

    Testando o sistema de token

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
             /*Insira nome do seu banco onde tem waterflow*/
-const sequelize = new Sequelize('test', 'root', 'sqllocal', {
+const sequelize = new Sequelize('test', 'root', 'cimatec', {
     host: 'localhost',
     dialect: 'mysql',
 });
