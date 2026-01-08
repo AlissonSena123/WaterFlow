959c46e579bfd9b51fccaad3bba230515165aa6e
Alisson <alissonsena784@gmail.com>
2026-01-08 10:54:27 -0300

    Testando o banco de dados

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('wtfl', 'dev', 'sqllocal', {
+const sequelize = new Sequelize('wtfl', 'root', 'sqllocal', {
     host: 'localhost',
     dialect: 'mysql',
 });
