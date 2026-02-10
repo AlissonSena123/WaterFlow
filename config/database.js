9c3caba2aefe8be982994b4f6176f92ad91d93a4
ALISSON OLIVEIRA SENA <021.889528@universidadesenaicimatec.edu.br>
2026-02-10 13:19:29 -0300

    Guardando vesão antiga do codigo

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('wtfl', 'root', 'sqllocal', {
+const sequelize = new Sequelize('wtfl', 'root', 'cimatec', {
     host: 'localhost',
     dialect: 'mysql',
 });
