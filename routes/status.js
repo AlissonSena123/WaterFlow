ead6f11f30623d72a3714c38f7ca6a44b6a31cc3
Alisson <alissonsena784@gmail.com>
2026-04-06 11:30:05 -0300

    Revisando codigos do projeto

--- routes/status.js
+++ routes/status.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/bairro", async (req, res) => {
-    console.log("✅ Rota /status/bairro chamada");
     try {
         const { data, error } = await supabase
             .from("abastecimento")   
