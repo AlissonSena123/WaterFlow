6488ff9ee92f1a4367aab1cf5fbdf3dd4a361cfb
Alisson <alissonsena784@gmail.com>
2026-04-29 20:56:27 -0300

    .

---
+++ public/js/utils/sidebar.js
@@ -0,0 +1,23 @@
+const sidebar = document.getElementById('sidebar');
+const iconHamburguer = document.getElementById('iconHamburguer');
+
+const overlay = document.createElement('div');
+overlay.id = 'sidebar-overlay';
+document.body.appendChild(overlay);
+
+function openSidebar() {
+    sidebar.classList.add('open');
+    overlay.classList.add('visible');
+}
+
+function closeSidebar() {
+    sidebar.classList.remove('open');
+    overlay.classList.remove('visible');
+}
+
+iconHamburguer.addEventListener('click', () => {
+    console.log("Clicou");
+    sidebar.classList.contains('open') ? closeSidebar() : openSidebar();
+});
+
+overlay.addEventListener('click', closeSidebar);
